970ec7dad33bf00fd7eebfd4776bd353ff61d887
vincer_yh <vincer.wjw@galaxyoversea.com>
2023-09-19 12:46:24 +0800

    再次修改后可用，并且使用了语法糖

--- dc-ui/src/components/Editor/BasicEditor.vue
+++ dc-ui/src/components/Editor/BasicEditor.vue
@@ -1,41 +1,39 @@
 <template>
-  <div>
-    <div style="border: 1px solid #ccc; margin-top: 10px">
-      <Toolbar
-          :editor="editorRef"
-          style="border-bottom: 1px solid #ccc"
-      />
-      <Editor
-          :defaultConfig="editorConfig"
-          v-model="valueHtml"
-          style="height: 400px; overflow-y: hidden"
-          @onCreated="handleCreated"
-      />
-    </div>
-    <div style="margin-top: 10px">
-      <textarea
-          v-model="valueHtml"
-          readonly
-          style="width: 100%; height: 200px; outline: none"
-      ></textarea>
-    </div>
-
+  <div class="editor-container">
+    <Toolbar class="editorToolBar"
+             :editor="editorRef"
+    />
+    <Editor
+        :defaultConfig="editorConfig"
+        v-model="valueHtml"
+        class="editorBody"
+        @onCreated="handleCreated"
+    />
   </div>
 </template>
 
 <script setup>
 import '@wangeditor/editor/dist/css/style.css';
-import { ref, shallowRef, defineExpose } from 'vue';
-import { Editor, Toolbar } from '@wangeditor/editor-for-vue';
+import {ref, shallowRef, defineExpose} from 'vue';
+import {Editor, Toolbar} from '@wangeditor/editor-for-vue';
+
 const editorRef = shallowRef();
 const valueHtml = ref(null);
-const editorConfig = { placeholder: '请输入内容...' };
+const editorConfig = {placeholder: '请输入内容...'};
 
 const handleCreated = (editor) => {
   console.log('子组件内部打印：子组件被创建');
   editorRef.value = editor;
 };
-
-
-defineExpose({ valueHtml });
+defineExpose({valueHtml});
 </script>
+<style scoped>
+
+.editorBody {
+  border: 1px solid #00ff00;
+}
+
+.editorToolBar {
+  border-bottom: 1px solid #ccc
+}
+</style>
