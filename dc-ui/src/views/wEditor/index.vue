7bc4851adfe3a784b65eca3c5fb390857a2592de
vincer <9205073+vincer-1555@user.noreply.gitee.com>
2023-09-24 11:55:45 +0800

    修改

--- dc-ui/src/views/wEditor/index.vue
+++ dc-ui/src/views/wEditor/index.vue
@@ -1,20 +1,22 @@
 <template>
-  <BasicEditor ref="wEditorRef"/>
-  <dig></dig>
+	<div>
+		<BasicEditor ref='wEditorRef' />
+		<dig></dig>
+	</div>
 </template>
 
 <script setup>
-import BasicEditor from "@/components/Editor/BasicEditor.vue";
-import Dig from "@/views/wEditor/C/dig.vue";
+import BasicEditor from '@/components/Editor/BasicEditor.vue';
+import Dig from '@/views/wEditor/C/dig.vue';
 
 const wEditorRef = ref();
 
 const getChildValueHtml = () => {
-  console.log("父页面获取到的值", wEditorRef.value.valueHtml);
+	console.log('父页面获取到的值', wEditorRef.value.valueHtml);
 };
 
 const setChildValueHtml = () => {
-  wEditorRef.value.valueHtml = "<p>初始内容</p>";
+	wEditorRef.value.valueHtml = '<p>初始内容</p>';
 };
 </script>
 
