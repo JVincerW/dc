2079181529e86add69883d9a10800d6d06162696
vincer <9205073+vincer-1555@user.noreply.gitee.com>
2023-09-25 21:01:33 +0800

    赋值给子元素的确认按钮

--- dc-ui/src/views/blogs/wEditor/index.vue
+++ dc-ui/src/views/blogs/wEditor/index.vue
@@ -1,39 +1,40 @@
 <template>
-    <div>
-        <el-button class="push" type="success" icon="Promotion" circle @click="getChildValueHtml"/>
-        <div>
-            <BasicEditor :editorData="editorData" ref="wEditorRef"/>
-            <dig ref="dig"></dig>
-        </div>
-    </div>
+	<div>
+		<el-button circle class='push' icon='Promotion' type='success' @click='getChildValueHtml' />
+		<div>
+			<BasicEditor ref='wEditorRef' :editorData='editorData' />
+			<dig ref='dig' :editorData='editorData'></dig>
+		</div>
+	</div>
 
 </template>
 
 <script setup>
 import BasicEditor from '@/components/Editor/BasicEditor.vue';
 import Dig from '@/views/blogs/wEditor/C/dig.vue';
-const wEditorRef = ref()
-const dig=ref()
+
+const wEditorRef = ref();
+const dig = ref();
 const editorData = {
-    isInit: true,
-    isOnlyRead: false,
-    valueHtml: '<p>初始内容</p>',
-    hasTitle: true,
-    title: "初始化标题"
+	isInit: true,
+	isOnlyRead: false,
+	valueHtml: '<p>初始内容</p>',
+	hasTitle: true,
+	title: '初始化标题',
 };
 
 const getChildValueHtml = () => {
-    console.log('父页面获取到的值', wEditorRef.value.initEditor);
-    dig.value.handleShow();
+	console.log('父页面获取到的值', wEditorRef.value.initEditor);
+	dig.value.handleShow();
 };
 
 </script>
 
 <style scoped>
 .push {
-    position: absolute;
-    bottom: 39px;
-    right: 80px;
-    z-index: 99;
+	position: absolute;
+	bottom: 39px;
+	right: 80px;
+	z-index: 99;
 }
 </style>
