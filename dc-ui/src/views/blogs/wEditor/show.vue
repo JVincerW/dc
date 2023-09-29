13697d95c5c78298db3eb20ce2e4766174254c44
vincer <9205073+vincer-1555@user.noreply.gitee.com>
2023-09-29 15:38:38 +0800

    文章列表功能完成

--- dc-ui/src/views/blogs/wEditor/show.vue
+++ dc-ui/src/views/blogs/wEditor/show.vue
@@ -1,22 +1,7 @@
 <template>
 	<div class='app-container'>
 		<el-form v-show='showSearch' ref='queryRef' :inline='true' :model='queryParams' label-width='68px'>
-			<el-form-item label='版本id' prop='uuid'>
-				<el-input
-						v-model='queryParams.uuid'
-						clearable
-						placeholder='请输入版本id'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
-			<el-form-item label='版本' prop='version'>
-				<el-input
-						v-model='queryParams.version'
-						clearable
-						placeholder='请输入版本'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
+			
 			<el-form-item label='文章标题' prop='title'>
 				<el-input
 						v-model='queryParams.title'
@@ -33,43 +18,12 @@
 						@keyup.enter='handleQuery'
 				/>
 			</el-form-item>
-			<el-form-item label='文章专属二维码地址' prop='qrcodePath'>
-				<el-input
-						v-model='queryParams.qrcodePath'
-						clearable
-						placeholder='请输入文章专属二维码地址'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
-			<el-form-item label='是否置顶' prop='top'>
-				<el-input
-						v-model='queryParams.top'
-						clearable
-						placeholder='请输入是否置顶'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
-			<el-form-item label='文章简介，最多200字' prop='description'>
-				<el-input
-						v-model='queryParams.description'
-						clearable
-						placeholder='请输入文章简介，最多200字'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
+			
 			<el-form-item label='文章关键字，优化搜索' prop='keywords'>
 				<el-input
 						v-model='queryParams.keywords'
 						clearable
 						placeholder='请输入文章关键字，优化搜索'
-						@keyup.enter='handleQuery'
-				/>
-			</el-form-item>
-			<el-form-item label='文章私密访问时的密钥' prop='password'>
-				<el-input
-						v-model='queryParams.password'
-						clearable
-						placeholder='请输入文章私密访问时的密钥'
 						@keyup.enter='handleQuery'
 				/>
 			</el-form-item>
@@ -127,9 +81,7 @@
 		
 		<el-table v-loading='loading' :data='articleList' @selection-change='handleSelectionChange'>
 			<el-table-column align='center' type='selection' width='55' />
-			<el-table-column align='center' label='${comment}' prop='id' />
-			<el-table-column align='center' label='版本id' prop='uuid' />
-			<el-table-column align='center' label='版本' prop='version' />
+			<el-table-column align='center' label='文章id' prop='id' />
 			<el-table-column align='center' label='文章标题' prop='title' />
 			<el-table-column align='center' label='用户ID' prop='userId' />
 			<el-table-column align='center' label='文章封面图片' prop='coverImage' width='100'>
@@ -137,18 +89,11 @@
 					<image-preview :height='50' :src='scope.row.coverImage' :width='50' />
 				</template>
 			</el-table-column>
-			<el-table-column align='center' label='当前文章阅读类型' prop='readType' />
-			<el-table-column align='center' label='当前文章适用的编辑器类型' prop='editorType' />
-			<el-table-column align='center' label='文章专属二维码地址' prop='qrcodePath' />
-			<el-table-column align='center' label='${comment}' prop='isMarkdown' />
-			<el-table-column align='center' label='文章内容' prop='content' />
-			<el-table-column align='center' label='markdown版的文章内容' prop='contentMd' />
 			<el-table-column align='center' label='是否置顶' prop='top' />
 			<el-table-column align='center' label='类型' prop='typeId' />
 			<el-table-column align='center' label='状态' prop='status' />
 			<el-table-column align='center' label='是否推荐' prop='recommended' />
 			<el-table-column align='center' label='是否原创' prop='original' />
-			<el-table-column align='center' label='文章简介，最多200字' prop='description' />
 			<el-table-column align='center' label='文章关键字，优化搜索' prop='keywords' />
 			<el-table-column align='center' label='是否开启评论' prop='comment' />
 			<el-table-column align='center' label='文章私密访问时的密钥' prop='password' />
@@ -168,8 +113,8 @@
 				@pagination='getList'
 		/>
 		
-		<!-- 添加或修改【请填写功能名称】对话框 -->
-		<el-dialog v-model='open' :title='title' append-to-body width='500px'>
+		<!-- 添加或修改【文章】对话框 -->
+		<el-dialog v-model='open' :title='title' append-to-body>
 			<el-form ref='articleRef' :model='form' :rules='rules' label-width='80px'>
 				<el-form-item label='版本id' prop='uuid'>
 					<el-input v-model='form.uuid' placeholder='请输入版本id' />
@@ -186,25 +131,28 @@
 				<el-form-item label='文章封面图片' prop='coverImage'>
 					<image-upload v-model='form.coverImage' />
 				</el-form-item>
-				<el-form-item label='文章专属二维码地址' prop='qrcodePath'>
+				<el-form-item label='二维码地址' prop='qrcodePath'>
 					<el-input v-model='form.qrcodePath' placeholder='请输入文章专属二维码地址' />
 				</el-form-item>
 				<el-form-item label='文章内容'>
 					<editor v-model='form.content' :min-height='192' />
 				</el-form-item>
-				<el-form-item label='markdown版的文章内容' prop='contentMd'>
+				<el-form-item label='markdown版内容' prop='contentMd'>
 					<el-input v-model='form.contentMd' placeholder='请输入内容' type='textarea' />
 				</el-form-item>
 				<el-form-item label='是否置顶' prop='top'>
 					<el-input v-model='form.top' placeholder='请输入是否置顶' />
 				</el-form-item>
-				<el-form-item label='文章简介，最多200字' prop='description'>
+				<el-form-item label='文章类型' prop='top'>
+					<el-input v-model='form.typeId' placeholder='请输入文章类型' />
+				</el-form-item>
+				<el-form-item label='文章简介' prop='description'>
 					<el-input v-model='form.description' placeholder='请输入文章简介，最多200字' />
 				</el-form-item>
-				<el-form-item label='文章关键字，优化搜索' prop='keywords'>
+				<el-form-item label='关键字' prop='keywords'>
 					<el-input v-model='form.keywords' placeholder='请输入文章关键字，优化搜索' />
 				</el-form-item>
-				<el-form-item label='文章私密访问时的密钥' prop='password'>
+				<el-form-item label='访问密钥' prop='password'>
 					<el-input v-model='form.password' placeholder='请输入文章私密访问时的密钥' />
 				</el-form-item>
 			</el-form>
@@ -268,7 +216,7 @@ const data = reactive({
 
 const { queryParams, form, rules } = toRefs(data);
 
-/** 查询【请填写功能名称】列表 */
+/** 查询【文章】列表 */
 function getList() {
 	loading.value = true;
 	listArticle(queryParams.value).then(response => {
@@ -337,7 +285,7 @@ function handleSelectionChange(selection) {
 function handleAdd() {
 	reset();
 	open.value = true;
-	title.value = '添加【请填写功能名称】';
+	title.value = '添加【文章】';
 }
 
 /** 修改按钮操作 */
@@ -347,7 +295,7 @@ function handleUpdate(row) {
 	getArticle(_id).then(response => {
 		form.value = response.data;
 		open.value = true;
-		title.value = '修改【请填写功能名称】';
+		title.value = '修改【文章】';
 	});
 }
 
@@ -375,7 +323,7 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
 	const _ids = row.id || ids.value;
-	proxy.$modal.confirm('是否确认删除【请填写功能名称】编号为"' + _ids + '"的数据项？').then(function() {
+	proxy.$modal.confirm('是否确认删除【文章】编号为"' + _ids + '"的数据项？').then(function() {
 		return delArticle(_ids);
 	}).then(() => {
 		getList();
