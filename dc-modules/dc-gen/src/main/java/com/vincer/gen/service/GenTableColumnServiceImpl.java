70d80f4647e7421c154c36ffcc7323b371f2dcdc
Vincer王继文 <vincer.wjw@galaxyoversea.com>
2023-11-02 09:57:13 +0800

    代码生成器修改

--- dc-modules/dc-gen/src/main/java/com/vincer/gen/service/GenTableColumnServiceImpl.java
+++ dc-modules/dc-gen/src/main/java/com/vincer/gen/service/GenTableColumnServiceImpl.java
@@ -1,6 +1,5 @@
 package com.vincer.gen.service;
 
-import com.vincer.common.core.text.Convert;
 import com.vincer.gen.domain.GenTableColumn;
 import com.vincer.gen.mapper.GenTableColumnMapper;
 import org.springframework.stereotype.Service;
@@ -29,36 +28,6 @@ public class GenTableColumnServiceImpl implements IGenTableColumnService {
         return genTableColumnMapper.selectGenTableColumnListByTableId(tableId);
     }
 
-    /**
-     * 新增业务字段
-     *
-     * @param genTableColumn 业务字段信息
-     * @return 结果
-     */
-    @Override
-    public int insertGenTableColumn(GenTableColumn genTableColumn) {
-        return genTableColumnMapper.insertGenTableColumn(genTableColumn);
-    }
 
-    /**
-     * 修改业务字段
-     *
-     * @param genTableColumn 业务字段信息
-     * @return 结果
-     */
-    @Override
-    public int updateGenTableColumn(GenTableColumn genTableColumn) {
-        return genTableColumnMapper.updateGenTableColumn(genTableColumn);
-    }
 
-    /**
-     * 删除业务字段对象
-     *
-     * @param ids 需要删除的数据ID
-     * @return 结果
-     */
-    @Override
-    public int deleteGenTableColumnByIds(String ids) {
-        return genTableColumnMapper.deleteGenTableColumnByIds(Convert.toLongArray(ids));
-    }
 }
